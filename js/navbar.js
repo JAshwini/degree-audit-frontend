7e95d6fd0d843b1b0a93ce3b8bceaa9a489d1795
JAshwini <joshiashwini39@gmail.com>
2017-10-26 10:08:12 +0530

    sprint 1-2 ui

---
+++ js/navbar.js
@@ -0,0 +1,40 @@
+function getProgramTableData() {
+  $.ajax({
+    url:'http://13.126.207.109:8080/DegreeAudit/controller?action=login&username=abhilash&password=abc123',
+    success: function (result) {
+      console.log(result);
+      $.ajax({
+        url:'http://13.126.207.109:8080/DegreeAudit/controller?action=getprogram',
+        dataType: 'json',
+        success: function(result) {
+          console.log(result);
+        }
+      });
+    }
+  });
+  
+}
+
+$("#courses").click(function(e){
+  e.preventDefault();
+  window.location.href='course_list.html';
+});
+
+
+function getPage(a){
+  if(a=='prgstr'){
+    $.ajax({
+      url:'programme_str.html',
+      success:function(data){
+        $("#dataarea").html(data);
+      }
+    });
+  }else if (a=='programme') {
+    $.ajax({
+      url:'programme.html',
+      success:function(data){
+        $("#dataarea").html(data);
+      }
+    });
+  }
+}
